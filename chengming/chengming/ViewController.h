4f61b848cd48aca1f77ceb83a75c3509a9707e83
mafeng1000 <ios-37@ACC80856.ipt.aol.com>
2015-10-30 11:13:51 +0800

    banben1
    新建项目

---
+++ chengming/chengming/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chengming
+//
+//  Created by iOS－37 on 15/10/30.
+//  Copyright © 2015年 iOS－37. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ chengming/chengming/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  chengming
+//
+//  Created by iOS－37 on 15/10/30.
+//  Copyright © 2015年 iOS－37. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
